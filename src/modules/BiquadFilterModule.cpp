675f37af0cbf3584f36c4e7531b7ba457bd20995
Alemagpie <fagnanialessio2004@gmail.com>
2026-04-21 18:09:26 +0200

    Start of biquad implem

--- src/modules/BiquadFilterModule.cpp
+++ src/modules/BiquadFilterModule.cpp
@@ -1,74 +1,26 @@
 #include "modules/BiquadFilterModule.h"
 
-void BiquadFilterModule::process(audio_block_t *block) {
-    if(!block) return;
-
-    int32_t b0, b1, b2, a1, a2, sum;
-	uint32_t in2, out2, bprev, aprev, flag;
-	uint32_t *data, *end;
-	int32_t *state;
-
-	end = (uint32_t *)(block->data) + AUDIO_BLOCK_SAMPLES/2;
-	state = (int32_t *)definition;
-
-	do {
-		b0 = *state++;
-		b1 = *state++;
-		b2 = *state++;
-		a1 = *state++;
-		a2 = *state++;
-		bprev = *state++;
-		aprev = *state++;
-		sum = *state & 0x3FFF;
-		data = end - AUDIO_BLOCK_SAMPLES/2;
-		do {
-			in2 = *data;
-			sum = signed_multiply_accumulate_32x16b(sum, b0, in2);
-			sum = signed_multiply_accumulate_32x16t(sum, b1, bprev);
-			sum = signed_multiply_accumulate_32x16b(sum, b2, bprev);
-			sum = signed_multiply_accumulate_32x16t(sum, a1, aprev);
-			sum = signed_multiply_accumulate_32x16b(sum, a2, aprev);
-			out2 = signed_saturate_rshift(sum, 16, 14);
-			sum &= 0x3FFF;
-			sum = signed_multiply_accumulate_32x16t(sum, b0, in2);
-			sum = signed_multiply_accumulate_32x16b(sum, b1, in2);
-			sum = signed_multiply_accumulate_32x16t(sum, b2, bprev);
-			sum = signed_multiply_accumulate_32x16b(sum, a1, out2);
-			sum = signed_multiply_accumulate_32x16t(sum, a2, aprev);
-			bprev = in2;
-			aprev = pack_16b_16b(
-				signed_saturate_rshift(sum, 16, 14), out2);
-			// retaining part of the sum is meant to implement the
-			// "first order noise shaping" described in this article:
-			// http://www.earlevel.com/main/2003/02/28/biquads/
-			//  TODO: is logical AND really correct, or maybe it
-			//        should really be signed_saturate_rshift() ???
-			sum &= 0x3FFF;
-			bprev = in2;
-			*data++ = aprev;
-		} while (data < end);
-        
-		flag = *state & 0x80000000;
-		*state++ = sum | flag;
-		*(state-2) = aprev;
-		*(state-3) = bprev;
-	} while (flag);
+void BiquadFilterModule::setCoeff(float f) {
+	w0 = 2.0f * PI * f / AUDIO_SAMPLE_RATE;
+	alpha = sin(w0) / (2.0f * Q);	//this is wrong for shelf filters
 }
 
-void BiquadFilterModule::setCoefficients(uint32_t stage, const int *coefficients)
-{
-	if (stage >= 4) return;
-	int32_t *dest = definition + (stage << 3);
-	__disable_irq();
-	if (stage > 0) *(dest - 1) |= 0x80000000;
-	*dest++ = *coefficients++;
-	*dest++ = *coefficients++;
-	*dest++ = *coefficients++;
-	*dest++ = *coefficients++ * -1;
-	*dest++ = *coefficients++ * -1;
-	//*dest++ = 0;
-	//*dest++ = 0;  // clearing filter state causes loud pop
-	dest += 2;
-	*dest   &= 0x80000000;
-	__enable_irq();
+void BiquadFilterModule::process(int16_t& sample) {
+	//y[n] = b0*x[n] + b1 * x[n-1] + b2* x[n-2] - a1 * y[n-1] - a2 * y[n-2]
+    int16_t out = 0;
+	int16_t curr_input = sample;
+
+	out += signed_saturate_rshift(curr_input * b0, 16, 15);
+	out += signed_saturate_rshift(prevInput * b1, 16, 15);
+	out += signed_saturate_rshift(prevPrevInput * b2, 16, 15);
+	out += signed_saturate_rshift(prevOutput * a1, 16, 15);
+	out += signed_saturate_rshift(prevPrevOutput * a2, 16, 15);
+
+	prevPrevInput = prevInput;
+	prevInput = sample;
+	prevPrevOutput = prevOutput;
+	prevOutput = out;
+
+	sample = out;
 }
+
