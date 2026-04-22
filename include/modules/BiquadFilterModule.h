e56eeb2727299729a7d0b4fc33a53b690f256d1b
Alemagpie <fagnanialessio2004@gmail.com>
2026-04-22 18:53:44 +0200

    Biquad implem

--- include/modules/BiquadFilterModule.h
+++ include/modules/BiquadFilterModule.h
@@ -9,6 +9,7 @@ class BiquadFilterModule : Module {
     public:
 	BiquadFilterModule() { reset(); }
 
+	void setCutoff(float f);
 	void setCoeff(float f);
 	void setHighPass(float f);
 	void setLowPass(float f);
@@ -17,13 +18,26 @@ class BiquadFilterModule : Module {
 	void setLowShelf(float f);
 	void setHighShelf(float f);
 
+	void setMode(uint8_t m);
+
     void process(int16_t& sample);
     inline void reset() override {  }
 
     private:
 	int16_t prevPrevInput, prevInput, prevPrevOutput, prevOutput;
 	int32_t b0, b1, b2, a1, a2;
-	float w0, alpha, Q, S, dBGain, a0;
+	float w0, sinw0, cosw0, alpha, Q, S, dBGain, a0;
+	const float SCALE = 536870912.0f; 	//To go from float to Q29
+
+	enum mode {
+		LOWPASS = 0,
+		HIGHPASS = 1,
+		NOTCH = 2,
+		BANDPASS = 3,
+		LOWSHELF = 4,
+		HIGHSHELF = 5
+	};
+	mode status = LOWPASS;
 };
 
 #endif
--- src/modules/BiquadFilterModule.cpp
+++ src/modules/BiquadFilterModule.cpp
@@ -1,20 +1,129 @@
 #include "modules/BiquadFilterModule.h"
 
+void BiquadFilterModule::setMode(uint8_t m) {
+	if(m < 6) {
+		switch(m) {
+			case 0:
+				status = LOWPASS;
+			break;
+
+			case 1:
+				status = HIGHPASS;
+			break;
+
+			case 2:
+				status = NOTCH;
+			break;
+
+			case 3:
+				status = BANDPASS;
+			break;
+
+			case 4:
+				status = LOWSHELF;
+			break;
+
+			case 5:
+				status = HIGHSHELF;
+			break;
+		}
+	}
+}
+
+void BiquadFilterModule::setCutoff(float f) {
+	setCoeff(f);
+
+	switch(status) {
+		case 0:
+			setLowPass(f);
+		break;
+
+		case 1:
+			setHighPass(f);
+		break;
+
+		case 2:
+			setNotch(f);
+		break;
+
+		case 3:
+			setBandPass(f);
+		break;
+
+		case 4:
+			setLowShelf(f);
+		break;
+
+		case 5:
+			setHighShelf(f);
+		break;
+
+		default:
+		break;
+	}
+}
+
 void BiquadFilterModule::setCoeff(float f) {
 	w0 = 2.0f * PI * f / AUDIO_SAMPLE_RATE;
+	sinw0 = sin(w0);
+	cosw0 = cos(w0);
 	alpha = sin(w0) / (2.0f * Q);	//this is wrong for shelf filters
+	a0 = 1.0f + alpha;
+}
+
+void BiquadFilterModule::setHighPass(float f) {
+	b0 = (int32_t)(((1.0f + cosw0) / 2.0f) / a0 * SCALE);
+	b1 = (int32_t)(-(1.0f + cosw0) / a0 * SCALE);
+	b2 = b0;
+	a1 = (int32_t)((-2.0f * cosw0) / a0 * SCALE);
+	a2 = (int32_t)(( 1.0f - alpha) / a0 * SCALE);
+}
+
+void BiquadFilterModule::setLowPass(float f) {
+	b0 = (int32_t)(((1.0f - cosw0) / 2.0f) / a0 * SCALE);
+	b1 = (int32_t)(( 1.0f - cosw0) / a0 * SCALE);
+	b2 = b0;
+	a1 = (int32_t)((-2.0f * cosw0) / a0 * SCALE);
+	a2 = (int32_t)(( 1.0f - alpha) / a0 * SCALE);
+}
+
+void BiquadFilterModule::setNotch(float f) {
+	b0 = (int32_t)(1.0f/ a0 * SCALE);
+	b1 = (int32_t)((-2.0f * cosw0) / a0 * SCALE);
+	b2 = b0;
+	a1 = (int32_t)((-2.0f * cosw0) / a0 * SCALE);
+	a2 = (int32_t)(( 1.0f - alpha) / a0 * SCALE);
+}
+
+void BiquadFilterModule::setBandPass(float f) {
+	b0 = (int32_t)(( sinw0 / 2.0f) / a0 * SCALE);
+	b1 = 0;
+	b2 = (int32_t)((-sinw0 / 2.0f) / a0 * SCALE);
+	a1 = (int32_t)((-2.0f * cosw0) / a0 * SCALE);
+	a2 = (int32_t)(( 1.0f - alpha) / a0 * SCALE);
+}
+
+void BiquadFilterModule::setLowShelf(float f) {
+	//Not yet implemented, need more calculations as well as shelf slope (s)
+}
+
+void BiquadFilterModule::setHighShelf(float f) {
+	//Not yet implemented, need more calculations as well as shelf slope (s)
 }
 
 void BiquadFilterModule::process(int16_t& sample) {
 	//y[n] = b0*x[n] + b1 * x[n-1] + b2* x[n-2] - a1 * y[n-1] - a2 * y[n-2]
-    int16_t out = 0;
+    int64_t sum = 0;
+	int16_t out;
 	int16_t curr_input = sample;
 
-	out += signed_saturate_rshift(curr_input * b0, 16, 15);
-	out += signed_saturate_rshift(prevInput * b1, 16, 15);
-	out += signed_saturate_rshift(prevPrevInput * b2, 16, 15);
-	out += signed_saturate_rshift(prevOutput * a1, 16, 15);
-	out += signed_saturate_rshift(prevPrevOutput * a2, 16, 15);
+	sum += (int64_t) b0 * curr_input;
+	sum += (int64_t) b1 * prevInput;
+	sum += (int64_t) b2 * prevPrevInput;
+	sum -= (int64_t) a1 * prevOutput;
+	sum -= (int64_t) a2 * prevPrevOutput;
+
+	out = signed_saturate_rshift(sum, 16, 29);
 
 	prevPrevInput = prevInput;
 	prevInput = sample;
